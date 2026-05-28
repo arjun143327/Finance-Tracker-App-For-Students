0051eb716ef00272feac42f675d96e9ae3c0077c
arjun143327 <arjunswaminathan03@gmail.com>
2026-05-28 12:37:36 +0530

    feat: add CategoryModel for data serialization and update flutter_local_notifications dependency

--- lib/data/models/category_model.dart
+++ lib/data/models/category_model.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+final Map<int, IconData> _iconRegistry = {
+  Icons.restaurant_rounded.codePoint: Icons.restaurant_rounded,
+  Icons.directions_bus_rounded.codePoint: Icons.directions_bus_rounded,
+  Icons.shopping_bag_rounded.codePoint: Icons.shopping_bag_rounded,
+  Icons.receipt_long_rounded.codePoint: Icons.receipt_long_rounded,
+  Icons.medical_services_rounded.codePoint: Icons.medical_services_rounded,
+  Icons.school_rounded.codePoint: Icons.school_rounded,
+  Icons.movie_rounded.codePoint: Icons.movie_rounded,
+  Icons.category_rounded.codePoint: Icons.category_rounded,
+};
+
 class CategoryModel {
   final int? id;
   final String name;
@@ -9,8 +20,8 @@ class CategoryModel {
   CategoryModel({
     this.id,
     required this.name,
-    this.icon = Icons.category_rounded,
-    this.color = Colors.blue,
+    required this.icon,
+    required this.color,
   });
 
   Map<String, dynamic> toMap() {
@@ -26,9 +37,8 @@ class CategoryModel {
     return CategoryModel(
       id: map['id'],
       name: map['name'],
-      icon: IconData(int.parse(map['icon'] ?? '0xe148'), fontFamily: 'MaterialIcons'),
+      icon: _iconRegistry[int.parse(map['icon'] ?? '${Icons.category_rounded.codePoint}')] ?? Icons.category_rounded,
       color: Color(map['color'] as int? ?? Colors.blue.toARGB32()),
     );
   }
 }
-
